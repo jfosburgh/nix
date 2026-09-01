005296a583cfe3ac62469b1497fcf4390e76b79b
James Fosburgh <jwfosburgh@gmail.com>
2026-08-31 22:43:15 -0400

    zram tuning

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -20,5 +20,15 @@
     ];
 
     system.stateVersion = "26.05";
+
+    # Compressed swap in RAM. zstd averages around 3:1, so even a full device
+    # occupies roughly a third of RAM. Sized to all of RAM so reclaim never
+    # has to spill to a slower disk swap.
+    zramSwap = {
+      enable = true;
+      algorithm = "zstd";
+      memoryPercent = 100;
+      priority = 100;
+    };
   };
 }
